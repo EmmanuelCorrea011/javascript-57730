9899e4fe001be2c8395add7f042f3752a51759cf
Emmanuel Correa <emmanuel.correa011@gmail.com>
2024-06-10 22:34:57 -0300

    primera clase

---
+++ app.js
@@ -0,0 +1,41 @@
+//comentar facilito doble "//" o */ y /*
+
+// let nombreXXX = "juan carlos" ;
+// // otra forma de crear variables es con "const" ejemplo :   const apellidoXXX = "carlitero"
+
+// // alert("prueba de alerta falopa mostrame el nombre")
+// // alert(nombreXXX)
+
+// console.log(nombreXXX);
+//monstrar nombre en consola//
+
+// const documento = 4567897;
+
+// let numero = prompt("escribir un mnumero"); // almacenar un variable
+// console.log(numero);
+// -----------------------------------------------------------
+
+// // let nombre = prompt ("ingresa tu nombre") ;
+// // console.log("HOLA "+ nombre +" COMO ESTAS?");
+// // alert("HOLA "+ nombre +" COMO ESTAS?");
+
+// -------------------------------------------------------------
+
+// como se usa el + con numeros y strings//
+// let n1 = 2
+// let n2 = 5
+
+// let total = n1 + n2;
+// console.log(total)
+
+let n3 = Number(prompt("ingresa un numero"));
+
+let n4 = Number(prompt("ingresa un segundo numero"));
+
+sdfsdf;
+
+console.log(n3 > n4); //booleano true or false//
+
+let total2 = n3 + n4; //suma de
+
+console.log(total2);
